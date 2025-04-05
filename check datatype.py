aa10d1b5520b6ea0cafc9e8799935fa3b38e9167
Optimus3264 <bhushanpatil096@gmail.com>
2025-04-05 22:50:48 +0530

    Add files via upload

---
+++ check datatype.py
@@ -0,0 +1,7 @@
+import numpy as np
+arr=np.array([1,2,3,4]) #int
+
+print(arr.dtype) #dtype used for checking data type
+
+arr2=np.array(['apple','banana','cherry']) #string
+print(arr2.dtype) #dtype used for checking data type
