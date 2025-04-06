c9430f6bbf2fceb81397116d3030e2c21b39be4b
Optimus3264 <bhushanpatil096@gmail.com>
2025-04-06 22:43:49 +0530

    Add files via upload

---
+++ data_types_in_numpy.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+# integer array
+arr_int=np.array([1,2,3,4], dtype=np.int32)
+print(arr_int)
+
+
+# float array
+arr_fl=np.array([1.2,2.3,3.8], dtype=np.float64)
+print(arr_fl)
+
+
+# boolean array
+arr_bl=np.array([True,False,True], dtype=np.bool_)
+print(arr_bl)
+
+
+# string array
+arr_str=np.array(['apple','banana','cherry'], dtype=np.bytes_)
+print(arr_str)
