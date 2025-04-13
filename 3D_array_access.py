702ea237b614f413dafaf7ad36cdb761213d9bab
Optimus3264 <bhushanpatil096@gmail.com>
2025-04-13 22:40:17 +0530

    Add files via upload

---
+++ 3D_array_access.py
@@ -0,0 +1,5 @@
+import numpy as np
+
+arr=np.array([[[1,2,3],[4,5,6]],[[7,8,9],[10,11,12]]])
+
+print(arr[1,0,2]) #1 means 2nd sub array[7,8,9][10,11,12], 0 means 1st mini array within sub array and 2 means 3nd index within mini array=9 
